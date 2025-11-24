d97e952e7e6bea4dccb6952ab68575ed725493c6
WangTingZe <kingpin58@aliyun.com>
2025-11-24 23:25:43 +0800

    add different platform's implementations

--- internal/platform.cpp
+++ internal/platform.cpp
@@ -22,23 +22,6 @@
 
 #include "platform.h"
 
-#if __has_include(<base/threading/platform_thread.h>)
-#ifndef HAS_LIBCHROME_LIBRARAY
-#define HAS_LIBCHROME_LIBRARAY
-#endif
-#endif
-
-#ifdef HAS_LIBCHROME_LIBRARAY
-#include <base/threading/platform_thread.h>
-#include <base/strings/sys_string_conversions.h>
-#include <base/strings/utf_string_conversions.h>
-#include <base/base64.h>
-#include <base/strings/string_split.h>
-#include <base/strings/string_util.h>
-#include <base/debug/stack_trace.h>
-#pragma comment(lib, "libChromeBase.lib")
-#endif
-
 #include <algorithm>
 #include <ctime>
 #include <chrono>
@@ -49,11 +32,17 @@
 #include <stacktrace>
 #endif
 
-#ifdef _WIN32
+#ifdef WINDOWS_OS
 #include <windows.h>
 // The SetThreadDescription API was brought in version 1607 of Windows 10.
 typedef HRESULT( WINAPI* SetThreadNameDescription )( HANDLE hThread,
    PCWSTR lpThreadDescription );
+#endif
+
+#if defined(ANDROID_OS) || defined(LINUX_OS)
+#include <pthread.h>
+#include <sys/prctl.h>
+#include <unistd.h>
 #endif
 
 #include "../log_util.h"
@@ -64,12 +53,8 @@ namespace framework
 std::wstring convert_to_wstring( std::string a_str )
 {
     std::wstring ret_str;
-#ifdef HAS_LIBCHROME_LIBRARAY
-    ret_str = base::SysNativeMBToWide( a_str );
-    return ret_str;
-#else
 
-#ifdef _WIN32
+#ifdef WINDOWS_OS
     if( a_str.empty() )
         return std::wstring();
 
@@ -85,8 +70,6 @@ std::wstring convert_to_wstring( std::string a_str )
     return ret_str;
 #endif
 
-#endif
-
     LogUtilFatal() << "No implementation!";
     return ret_str;
 }
@@ -94,12 +77,8 @@ std::wstring convert_to_wstring( std::string a_str )
 std::string convert_to_string( std::wstring a_str )
 {
     std::string str;
-#ifdef HAS_LIBCHROME_LIBRARAY
-    str = base::SysWideToNativeMB( a_str );
-    return str;
-#else
 
-#ifdef _WIN32
+#ifdef WINDOWS_OS
     int wide_length = static_cast< int >( a_str.length() );
     if( wide_length == 0 )
         return std::string();
@@ -116,19 +95,38 @@ std::string convert_to_string( std::wstring a_str )
     return str;
 #endif
 
-#endif
     LogUtilFatal() << "No implementation!";
     return str;
 }
 
+#ifdef WINDOWS_OS
+typedef struct tagTHREADNAME_INFO {
+    DWORD dwType;  // Must be 0x1000.
+    LPCSTR szName;  // Pointer to name (in user addr space).
+    DWORD dwThreadID;  // Thread ID (-1=caller thread).
+    DWORD dwFlags;  // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+static void set_current_thread_name( const char* a_name )
+{
+    const DWORD kVCThreadNameException = 0x406D1388;
+    THREADNAME_INFO info;
+    info.dwType = 0x1000;
+    info.szName = a_name;
+    info.dwThreadID = ::GetCurrentThreadId();
+    info.dwFlags = 0;
+    __try {
+        RaiseException( kVCThreadNameException, 0, sizeof( info ) / sizeof( DWORD ),
+            reinterpret_cast<DWORD_PTR*>(&info) );
+    }
+    __except (EXCEPTION_CONTINUE_EXECUTION) {
+    }
+}
+#endif
+
 void set_thread_name( const std::string& a_name )
 {
-#ifdef HAS_LIBCHROME_LIBRARAY
-    base::PlatformThread::SetName( a_name );
-    return;
-#else
 
-#ifdef _WIN32
+#ifdef WINDOWS_OS
     // The SetThreadDescription API works even if no debugger is attached.
     static auto set_thread_description_func =
         reinterpret_cast< SetThreadNameDescription >( ::GetProcAddress(
@@ -142,14 +140,28 @@ void set_thread_name( const std::string& a_name )
     }
     else
     {
-        LogUtilError() << "This windows version cannot set thread name. But can throw an exception to"
-            " set thread name. Since just old windows need this step, so we are not going to make an "
-            "implementation for this.";
+        set_current_thread_name( a_name.c_str() );
         return;
     }
 #endif
 
-#endif  
+#if defined(ANDROID_OS) || defined(LINUX_OS)
+    // Like linux, on android we can get the thread names to show up in the
+    // debugger by setting the process name for the LWP.
+    // We don't want to do this for the main thread because that would rename
+    // the process, causing tools like killall to stop working.
+    if (get_current_thread_id() == getpid())
+        return;
+
+    // Set the name for the LWP (which gets truncated to 15 characters).
+    int err = prctl( PR_SET_NAME, name.c_str() );
+    if (err < 0 && errno != EPERM)
+    {
+        LogUtilError() << "cannot set current thread's name";
+    }
+    return;
+#endif
+
     LogUtilFatal() << "No implementation!";
 }
 
@@ -166,28 +178,36 @@ uint64_t get_time_stamp()
 
 uint64_t get_current_thread_id()
 {
-#ifdef _WIN32
+#if defined(MACOSX)
+    return pthread_mach_thread_np( pthread_self() );
+#elif defined(LINUX_OS)
+    return syscall( __NR_gettid );
+#elif defined(ANDROID_OS)
+    return gettid();
+#elif defined(FUCHSIA_OS)
+    return zx_thread_self();
+#elif defined(SOLARIS_OS) || defined(QNX)
+    return pthread_self();
+#elif defined(NACL_OS) && defined(__GLIBC__)
+    return pthread_self();
+#elif defined(NACL_OS) && !defined(__GLIBC__)
+    // Pointers are 32-bits in NaCl.
+    return reinterpret_cast<int32_t>(pthread_self());
+#elif defined(POSIX_OS) && defined(AIX_OS)
+    return pthread_self();
+#elif defined(POSIX_OS) && !defined(AIX_OS)
+    return reinterpret_cast<int64_t>(pthread_self());
+#elif defined(WINDOWS_OS)
     static thread_local uint64_t tid = ::GetCurrentThreadId();
+    return tid;
 #else
     LogUtilFatal() << "No implementation!";
 #endif
-    return tid;
 }
 
 std::u8string convert( std::string const& a_source )
 {
-#ifdef HAS_LIBCHROME_LIBRARAY
-    std::wstring w_str = base::SysNativeMBToWide( a_source );
-    std::string utf8_str = base::SysWideToUTF8( w_str );
-
-    std::u8string::value_type const* _str = nullptr;
-    _str = reinterpret_cast< std::u8string::value_type const* >( utf8_str.c_str() );
-    std::u8string str( _str, utf8_str.size() + 1 );
-
-    return str;
-#else
-
-#ifdef _WIN32
+#ifdef WINDOWS_OS
     std::u8string str;
     std::wstring w_str = convert_to_wstring( a_source );
     int wide_length = static_cast< int >( w_str.length() );
@@ -209,14 +229,12 @@ std::u8string convert( std::string const& a_source )
     str = std::u8string( _str, utf8_str.size() + 1 );
     return str;
 #endif
-
-#endif
     LogUtilFatal() << "No implementation!";
 }
 
 std::string convert( std::u8string const& a_source )
 {
-#if defined(_WIN32)
+#if defined(WINDOWS_OS)
     // UTF-8 -> UTF-16
     int wlen = MultiByteToWideChar( CP_UTF8, 0,
         reinterpret_cast<const char*>(a_source.c_str()),
@@ -241,13 +259,6 @@ std::string convert( std::u8string const& a_source )
 std::string current_call_stack()
 {
     std::string call_stack;
-#ifdef HAS_LIBCHROME_LIBRARAY
-    std::stringstream ss;
-    std::string logstr;
-    base::debug::StackTrace trace;
-    trace.OutputToStream(&ss);
-    call_stack = ss.str();
-#else
 
 #if __has_include(<stacktrace>)
 #ifdef __cpp_lib_stacktrace
@@ -262,27 +273,18 @@ std::string current_call_stack()
 #endif
 #endif
 
-#endif
     return call_stack;
 }
 
 void base64_encode( char const* a_buffer, uint16_t a_size, std::string* output )
 {
-#ifdef HAS_LIBCHROME_LIBRARAY
-    base::Base64Encode( a_buffer, a_size, output );
-    return;
-#endif
     LogUtilFatal() << "No implementation!";
 }
 
 bool base64_decode( const std::string& input, std::vector<char>& output )
 {
-#ifdef HAS_LIBCHROME_LIBRARAY
-    return base::Base64Decode( input, output );
-#else
     LogUtilFatal() << "No implementation!";
     return false;
-#endif
 }
 
 }
--- internal/platform.h
+++ internal/platform.h
@@ -27,6 +27,68 @@
 
 #include "../framework_export.h"
 
+// A set of macros to use for platform detection.
+#if defined(__native_client__)
+// __native_client__ must be first, so that other OS_ defines are not set.
+#define NACL_OS 1
+// OS_NACL comes in two sandboxing technology flavors, SFI or Non-SFI.
+// PNaCl toolchain defines __native_client_nonsfi__ macro in Non-SFI build
+// mode, while it does not in SFI build mode.
+#if defined(__native_client_nonsfi__)
+#define NACL_NONSFI_OS
+#else
+#define NACL_SFI_OS
+#endif
+#elif defined(__APPLE__)
+// only include TargetConditions after testing ANDROID as some android builds
+// on mac don't have this header available and it's not needed unless the target
+// is really mac/ios.
+#include <TargetConditionals.h>
+#define MACOSX 1
+#if defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
+#define IOS_OS 1
+#endif  // defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE
+#elif defined(__linux__)
+#define LINUX_OS 1
+// include a system header to pull in features.h for glibc/uclibc macros.
+#include <unistd.h>
+#if defined(__GLIBC__) && !defined(__UCLIBC__)
+// we really are using glibc, not uClibc pretending to be glibc
+#define LIBC_GLIBC 1
+#endif
+#elif defined(_WIN32)
+#define WINDOWS_OS 1
+#elif defined(__Fuchsia__)
+#define FUCHSIA_OS 1
+#elif defined(__FreeBSD__)
+#define FREEBSD_OS 1
+#elif defined(__NetBSD__)
+#define NETBSD_OS 1
+#elif defined(__OpenBSD__)
+#define OPENBSD_OS 1
+#elif defined(__sun)
+#define SOLARIS_OS 1
+#elif defined(__QNXNTO__)
+#define QNX_OS 1
+#elif defined(_AIX)
+#define AIX_OS 1
+#elif defined(__asmjs__)
+#define ASMJS_OS
+#elif defined(__ANDROID__)
+#define ANDROID_OS
+#else
+#error Please add support for your platform in build/build_config.h
+#endif
+
+// For access to standard POSIXish features, use POSIX instead of a
+// more specific macro.
+#if defined(AIX_OS) || defined(ANDROID_OS) || defined(ASMJS_OS) ||    \
+    defined(FREEBSD_OS) || defined(LINUX_OS) || defined(MACOSX_OS) || \
+    defined(NACL_OS) || defined(NETBSD_OS) || defined(OPENBSD_OS) ||  \
+    defined(QNX_OS) || defined(SOLARIS_OS)
+#define POSIX_OS 1
+#endif
+
 namespace framework
 {
 
